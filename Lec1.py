88af6f9a3d7e97d729fec82ffe91c5310b4e2b94
Bilal Shahid <bk114326@gmail.com>
2026-07-04 14:48:02 +0500

    Add variables

---
+++ Lec1.py
@@ -0,0 +1,9 @@
+# different types like string, integer, float, etc. In Python,
+# you can create a variable by simply assigning a value to it
+# using the equals sign (=).
+name = "Bilal Shahid" #String variable
+print(name)
+roll_no = 154
+print(roll_no) #Integer variable
+cgpa = 3.5 #Float variable
+print(cgpa)
