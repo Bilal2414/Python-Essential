e72e1e6db7221a11e824ce78721e07fd22fdf424
Bilal Shahid <bk114326@gmail.com>
2026-07-05 12:30:14 +0500

    Add some more basics

--- Lec1.py
+++ Lec1.py
@@ -13,3 +13,27 @@ students_marks = [85, 90, 78, 92]  # List variable
 print(students_names)
 print(students_marks)
 print('"Hello world!"')
+# Type Casting is the process of converting a variable from one
+#  type to another. 
+x =str(3)    # x will be '3'
+print(x)
+print(type(x))
+y = float(3)  # y will be 3.0
+print(y)
+print(type(y))
+z = int("3")    # z will be 3
+print(z)
+print(type(z))
+
+name = "Bilal Shahid"
+print(name[0]) # prints the first character of the string
+print(name[1]) # prints the second character of the string
+print(name[2]) # prints the third character of the string
+print(name[3]) # prints the fourth character of the string
+print(name[4]) # prints the fifth character of the string
+# Python allows you to assign values to multiple variables in one line
+x, y, z = 5, 10, 15
+print(x, y, z)
+# And you can assign the same value to multiple variables in one line
+a = b = c = 20
+print(a,b,c)
