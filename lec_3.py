adec903450fd802c731a4bdfd8b1404487873ee0
Bilal Shahid <bk114326@gmail.com>
2026-07-16 23:42:19 +0500

    Add list, Tuple, Dictionaries

---
+++ lec_3.py
@@ -0,0 +1,7 @@
+list_1 =["Bilal", "Shahid", "from", "Swabi"]
+print(list_1)
+list_1.append("Pakistan")
+print(list_1)
+list_2 = ["Karachi", "Lahore", "Islamabad"]
+list_1.extend(list_2)
+print(list_1)
