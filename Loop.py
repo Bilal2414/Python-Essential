b515200fdbe1cfdc09597c8502d6845979ef090c
Bilal Shahid <bk114326@gmail.com>
2026-07-17 23:00:39 +0500

    Add some values

---
+++ Loop.py
@@ -0,0 +1,14 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+for i in numbers:
+    print(i)
+
+for i in range(5):
+    print("Welcome")
+for i in range(0,21,2):
+    print(i)
+for i in range(10,0,-1):
+    print(i)
+for i in range(0,22,3):
+    print(i)
+for i in range(1,11):
+    print(i, "Square of i is", i**2)
--- lec_3.py
+++ lec_3.py
@@ -5,3 +5,12 @@ print(list_1)
 list_2 = ["Karachi", "Lahore", "Islamabad"]
 list_1.extend(list_2)
 print(list_1)
+list_1.insert(2, "is")
+print(list_1)
+list_1.remove("from")
+list_1.remove("Swabi")
+print(list_1)
+list_1.pop()
+print(list_1)
+list_1[0]= "Hello This is me Bilal"
+print(list_1)
