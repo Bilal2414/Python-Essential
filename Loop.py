c0d631eae7b32629fc84b3543b78a744ca38fe0c
Bilal Shahid <bk114326@gmail.com>
2026-07-17 23:01:22 +0500

    Add Loop

--- Loop.py
+++ Loop.py
@@ -1,3 +1,4 @@
+
 numbers = [1,2,3,4,5,6,7,8,9,10]
 for i in numbers:
     print(i)
@@ -12,3 +13,4 @@ for i in range(0,22,3):
     print(i)
 for i in range(1,11):
     print(i, "Square of i is", i**2)
+
